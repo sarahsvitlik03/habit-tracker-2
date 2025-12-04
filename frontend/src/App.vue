68b84b7ccbaaa4306d9ec5f3808caccacbc6be9e
sarahsvit <svitliksarah@gmail.com>
2025-12-04 17:08:43 -0500

    Update App.vue

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,12 +1,8 @@
 <script setup>
-/**
- * Import reactive state utilities from Vue.
- * - ref() lets variables be reactive
- * - onMounted() runs code when the component first loads
- */
 import { ref, onMounted } from "vue";
 
-/*   const chores = ref([
+/* Hard coded JSON Data -> Now pulling instead from MongoDB  
+  const chores = ref([
   { name: "Vacuuming", assigned: "Sarah", day: "Saturday", completed: false },
   { name: "Dishes", assigned: "Charlie", day: "Sunday", completed: false },
   { name: "Laundry", assigned: "Mom", day: "Monday", completed: true }
@@ -16,34 +12,15 @@ const chores = ref([]); //stores the list of chores
 const loading = ref(true); // Shows "Loading..." while fetching
 const error = ref(null); // Stores any error message
 
-// To Do List
-  // Add pop up for when status, assigned, or due date has been changed.
-  // Add pop for when new chore is added. 
-  // Get button working with MongoDB  
-  // Add a third status: late when you have date in the past or haven't checked it off
-  // Need a pop-up to insert new document
-  // add onClick function for add chore. Let it open up a v-if form in the middle of the page if clicked, add to mongodb, reload database 
-
-// Top To Do List Items
-  // Transfer all data to be updated in MongoDB 
-  // Add button to insert new document with all editable text
-
-/**
- * Fetch data from the Express.js backend when component loads.
- * This calls: http://localhost:3000/api/books
- */
-
 function addChore () {
-  //set a variable to true perhaps, then show pop-up if that is true
-  // OR it automatically inserts a completely blank chore that you can fill in. 
+  // inserts a completely blank chore that you can fill in. 
 }
+
  onMounted(async () => {
   try {
     const res = await fetch("http://localhost:3000/api/chores");
-
     // If the server sends a non-200 response
     if (!res.ok) throw new Error("Failed to fetch");
-
     // Convert response to JSON and store it in 'books'
     chores.value = await res.json();
   } catch (err) {
@@ -58,14 +35,13 @@ function addChore () {
 <template>
   <div class="page-container">
   <div class="top-line">
-  <h1>Household Chore Tracker</h1>
-  <button type="button"> Insert Chore</button>
-</div>
-      <!-- When loading -->
-      <p v-if="loading" class="status">Loading data...</p>
-      <!-- When an error occurs -->
-      <p v-if="error" class="error">{{ error }}</p>
-
+    <h1>Household Chore Tracker</h1>
+    <button type="button"> Insert Chore</button>
+  </div>
+  <!-- When loading -->
+    <p v-if="loading" class="status">Loading data...</p>
+    <!-- When an error occurs -->
+    <p v-if="error" class="error">{{ error }}</p>
   <div class="chores-grid"> 
     <div v-for="chore in chores" :key="chore.name" class="chores-card">
     <div class="container">
@@ -78,15 +54,13 @@ function addChore () {
       <p v-if="chore.assigned" contenteditable="true">{{ chore.assigned }}</p>
       <p v-else contenteditable="">Unassigned</p>
   </div>
-      <!-- Add alert that the database has been updated-->
-
-    <div class="date">
+  <div class="date">
     <p> Due Date:</p><input type="date"/>
   </div>
     <p>Status: {{ chore.completed ? "Done" : "Not yet" }}</p>
   </div>
-</div>
-</div>
+  </div>
+ </div>
 </div>
 </template>
 
@@ -130,3 +104,16 @@ function addChore () {
   margin-left: 10px;
 }
 </style>
+
+<!-- // To Do List
+  // Add pop up for when status, assigned, or due date has been changed.
+  // Add pop for when new chore is added. 
+  // Get button working with MongoDB  
+  // Add a third status: late when you have date in the past or haven't checked it off
+  // Need a pop-up to insert new document
+  // add onClick function for add chore. Let it open up a v-if form in the middle of the page if clicked, add to mongodb, reload database 
+
+// Top To Do List Items
+  // Transfer all data to be updated in MongoDB 
+  // Add button to insert new document with all editable text
+--> 
